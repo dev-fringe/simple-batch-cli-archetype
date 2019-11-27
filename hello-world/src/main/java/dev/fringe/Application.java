78eb83c9fa52618333fb3e52d58ee81d9c648a62
LEE HAK DO <dev.fringe@gmail.com>
2019-11-27 12:50:01 +0900

    init

--- hello-world/src/main/java/dev/fringe/Application.java
+++ hello-world/src/main/java/dev/fringe/Application.java
@@ -1,7 +1,5 @@
 package dev.fringe;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.UUID;
 
 import org.junit.Test;
@@ -26,15 +24,15 @@ public class Application extends JobSupport{
 	private static final String JOB2 = "Job2";
 	private static final String JOB3 = "Job3";
 	private static final String HELLO_WORLD_JOB = "helloWorldJob";
-	private static final List<String> JOBS = Arrays.asList(JOB2, HELLO_WORLD_JOB);
+//	private static final List<String> JOBS = Arrays.asList(JOB2, HELLO_WORLD_JOB);
 	
 	@Test 
 	public void testJobs(){
 		try {
-//			JobExecution execution = jobLauncher.run(context.getBean(HELLO_WORLD_JOB, Job.class), new JobParametersBuilder().addString("uid", UUID.randomUUID().toString()).toJobParameters());
-//			System.out.println("Exit Status : " + execution.getStatus());
-//			JobExecution execution2 = jobLauncher.run(context.getBean(JOB2, Job.class), new JobParametersBuilder().addString("uid", UUID.randomUUID().toString()).toJobParameters());
-//			System.out.println("Exit Status : " + execution2.getStatus());
+			JobExecution execution = jobLauncher.run(context.getBean(HELLO_WORLD_JOB, Job.class), new JobParametersBuilder().addString("uid", UUID.randomUUID().toString()).toJobParameters());
+			System.out.println("Exit Status : " + execution.getStatus());
+			JobExecution execution2 = jobLauncher.run(context.getBean(JOB2, Job.class), new JobParametersBuilder().addString("uid", UUID.randomUUID().toString()).toJobParameters());
+			System.out.println("Exit Status : " + execution2.getStatus());
 			JobExecution execution3 = jobLauncher.run(context.getBean(JOB3, Job.class), new JobParametersBuilder().addString("uid", UUID.randomUUID().toString()).toJobParameters());
 			System.out.println("Exit Status : " + execution3.getStatus());
 		} catch (BeansException | JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException | JobParametersInvalidException e) {
--- hello-world/src/main/java/dev/fringe/model/User2.java
+++ hello-world/src/main/java/dev/fringe/model/User2.java
@@ -3,14 +3,12 @@ package dev.fringe.model;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.GenericGenerator;
 
 import lombok.Data;
 
@@ -20,10 +18,7 @@ import lombok.Data;
 public class User2 {
 	
 	@Id
-//	@GeneratedValue(generator = "increment")
-//	@GenericGenerator(name = "increment", strategy = "increment")
 	private Long id;
-	private String ids;
 	private String name;
 	private String test;
 	@CreationTimestamp
